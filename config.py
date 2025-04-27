808e0b78c049f4191336ab73ad84cca4dcc2ce2f
Hᴇᴀʀᴛ_Tʜɪᴇꜰᴛ <151960878+Jeeva1055221@users.noreply.github.com>
2025-04-27 12:43:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -95,8 +95,8 @@ DIRECT_GEN_URL = os.environ.get("DIRECT_GEN_URL", "https://filestoresot.koyeb.ap
 DIRECT_GEN = bool(DIRECT_GEN_DB and DIRECT_GEN_URL)
 
 POST_MODE= bool(environ.get('POST_MODE', True))
-POST_SHORT_API = environ.get('POST_SHORT_API', 'gplinks.com')
-POST_SHORT_URL = environ.get('POST_SHORT_URL', '89c5f49bb30bf45ddae5d35314cc7525a36aceb0')
+POST_SHORT_URL = environ.get('POST_SHORT_URL', 'gplinks.com')
+POST_SHORT_API = environ.get('POST_SHORT_API', '89c5f49bb30bf45ddae5d35314cc7525a36aceb0')
 
 HOW_TO_POST_SHORT = environ.get('HOW_TO_POST_SHORT', 'https://t.me/Howtodowloa/13')
 
